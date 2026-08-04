fdf362f183108fe50694b5b8b29e4c9d46f844d7
Sathwik puttala <sathwikputtala2003@gmail.com>
2026-08-04 22:32:02 +0530

    project setup

---
+++ App.js
@@ -0,0 +1,7 @@
+import { View, Text,ImageBackground } from "react-native";
+import image from './assets/favicon.png'
+export default function App() {
+  return <View style={{ flex:1,padding: 100 }}>
+   <Text>Hello sathwik day 2</Text>
+  </View>
+}
---
+++ index.js
@@ -0,0 +1,8 @@
+import { registerRootComponent } from 'expo';
+
+import App from './All Codes/Coding-2/StyleSheetApi';
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
